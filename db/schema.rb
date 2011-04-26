1960491910b49afb831965bce312aa85646d5aa8
Massimo Bondanza <massimo.bondanza@gmail.com>
2011-04-26 15:46:19 +0200

    finito chap 11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110425092358) do
+ActiveRecord::Schema.define(:version => 20110425184601) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "companyname"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
